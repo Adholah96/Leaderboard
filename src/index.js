d27bf4bc79642700aeb21bbd776c20a561834dfc
Adholah96 <adholah96@gmail.com>
2023-03-09 20:20:59 +0300

    Add style to leaderboard

--- src/index.js
+++ src/index.js
@@ -1,47 +1,48 @@
-import './style.css';
-import postData from '../modules/postdata.js';
-import fetchData from '../modules/fetchdata.js';
-import displayData from '../modules/displayData.js';
+import './style.css'
+import postData from '../modules/postdata.js'
+import fetchData from '../modules/fetchdata.js'
+import displayData from '../modules/displayData.js'
 
-const refreshBtn = document.querySelector('.refreshbtn');
-const form = document.querySelector('.form');
-const nameInput = form.elements.name;
-const scoreInput = form.elements.score;
+const refreshBtn = document.querySelector('.refreshbtn')
+const form = document.querySelector('.form')
+const nameInput = form.elements.name
+const scoreInput = form.elements.score
 
 refreshBtn.addEventListener('click', async () => {
-  displayData(await fetchData());
-});
+  displayData(await fetchData())
+})
 
 const addScore = async (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
-  const result = await postData(nameInput.value, scoreInput.value);
+  const result = await postData(nameInput.value, scoreInput.value)
 
   if (result) {
-    nameInput.value = '';
-    scoreInput.value = '';
+    nameInput.value = ''
+    scoreInput.value = ''
 
-    displayData(await fetchData());
+    displayData(await fetchData())
   }
-};
+}
 const init = async () => {
-  const data = await fetchData();
+  const data = await fetchData()
   if (data) {
-    displayData(data);
+    displayData(data)
   }
 
   if (form) {
-    form.addEventListener('submit', addScore);
+    form.addEventListener('submit', addScore)
+    form.clear()
   }
 
   if (refreshBtn) {
     refreshBtn.addEventListener('click', async () => {
-      const data = await fetchData();
+      const data = await fetchData()
       if (data) {
-        displayData(data);
+        displayData(data)
       }
-    });
+    })
   }
-};
+}
 
-init();
+init()
